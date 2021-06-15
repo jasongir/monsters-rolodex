e756737dc52f4ee565b77d955d3daab82e70a3e9
Jason <snujugroix@gmail.com>
2021-06-15 14:41:25 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+/**
+ * ! how this got hosted:
+ * - make new github repo
+ * git remote add origin https://github.com/jasongir/asdfkj.git
+ * yarn add gh-pages
+ *
+ * add to package.json:
+ * 	"homepage": "https://jasongir.github.io/monsters-rolodex",
+ * under scripts:
+ *    "predeploy": "yarn build",
+ *    "deploy": "gh-pages -d build"
+ *
+ */
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: [],
+			searchField: "",
+		};
+
+		// this.handleChange = this.handleChange.bind(this);
+	}
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
+
+	handleChange = (e) => {
+		// arrow functions let us bind this to the context of the arrow function, so no need for .bind
+		this.setState({
+			searchField: e.target.value,
+		});
+	};
+
+	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+		// const monsters = this.state.monsters
+		// const searchField = this.state.searchField
+
+		return (
+			<div className="App">
+				<h1>Monsters Rolodex</h1>
+				<SearchBox
+					placeholder="search monsters"
+					handleChange={this.handleChange}
+				/>
+
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
 }
 
 export default App;
+
+// function App() {
+// 	return (
+// 		<main className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>Hello! my name is joseph mother</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 			</header>
+// 		</main>
+// 	);
+// }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./card-list.styles.css";
+import { Card } from "../card/card.component.jsx";
+
+export const CardList = (props) => {
+	// console.log(props);
+	return (
+		<div className="card-list">
+			{props.monsters.map((monster) => (
+				<Card key={monster.id} monster={monster} />
+			))}
+		</div>
+	);
+};
